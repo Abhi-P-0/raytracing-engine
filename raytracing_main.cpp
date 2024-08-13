75ba3a27428b77ffb75873996d09946ad8ec900f
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2024-08-12 23:00:01 -0400

    setup camera and modified main classes functionality

--- raytracing_main.cpp
+++ raytracing_main.cpp
@@ -2,24 +2,12 @@
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 const int screenSize = 256;
 
 
 
-color ray_color(const ray& r, const hittable& world) {
-    hit_record rec;
-
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
 
 int main() {
     // Image
@@ -30,11 +18,17 @@ int main() {
     int image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // World
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 400;
+
+    cam.render(world);
 
     // Camera
     auto focal_length = 1.0;
--- rtweekend.h
+++ rtweekend.h
@@ -6,6 +6,11 @@
 #include <limits>
 #include <memory>
 
+#include "color.h"
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
 
 // C++ Std Usings
 
